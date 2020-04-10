bc25924bec1e367b428c8bd49f1ef1560e19723d
jstfoll <bharuka23@gmail.com>
2020-04-11 03:02:52 +0530

    login complete

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+def user_unicode_patch(self):
+    return '%s %s' % (self.first_name, self.last_name)
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages  #for flash messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -12,9 +13,14 @@ def register(request):
             username= form.cleaned_data.get('username')
             form.save()
             messages.success(request, f'Account created for { username } !!')
-            return redirect('home')
+            return redirect('login')
     else:
         form= UserRegisterForm()
 
     context = {'form': form , 'title': 'Register-Blog'}
     return render(request, 'users/register.html' , context)
+
+@login_required
+def profile(request):
+    context = {'title': 'Register-Blog'}
+    return render(request, 'users/profile.html' , context)
